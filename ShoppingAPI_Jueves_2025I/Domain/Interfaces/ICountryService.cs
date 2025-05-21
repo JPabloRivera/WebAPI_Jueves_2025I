81913b467554789d17fa3b3cbd45ec3609f1b3d2
JPabloRivera <jpstileo@gmail.com>
2025-05-20 22:36:55 -0500

    Services layer developed

--- ShoppingAPI_Jueves_2025I/Domain/Interfaces/ICountryService.cs
+++ ShoppingAPI_Jueves_2025I/Domain/Interfaces/ICountryService.cs
@@ -4,11 +4,12 @@ namespace ShoppingAPI_Jueves_2025I.Domain.Interfaces
 {
     public interface ICountryService
     {
-        Task<IEnumerable<Country>> GetCountriesAsync(); // Una de las tantas firmas de un método
-
-        Task<Country> CreateCountryAsync(Country country);
+        // Una de las tantas firmas de un método
+        Task<IEnumerable<Country>> GetCountriesAsync(); 
 
         Task<Country> GetCountryByIdAsync(Guid id);
+
+        Task<Country> CreateCountryAsync(Country country);
 
         Task<Country> EditCountryAsync(Country country);
 
